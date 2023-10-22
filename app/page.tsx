9ab3ab43f2635a87db5ab64f2888dbdead49bd86
Logan Davidson <logan.davidson@autorek.com>
2023-10-22 13:26:32 +0100

    Add athlete selection for leaderboard

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,12 @@ import downloadRecords from "@/functions/downloadRecords"
 import { DataGrid } from "@mui/x-data-grid"
 import { formatDate } from "@/functions/formatDate"
 import {
+  Button,
+  Chip,
   CircularProgress,
+  Dialog,
   FormControl,
+  IconButton,
   InputLabel,
   MenuItem,
   Select,
@@ -20,7 +24,10 @@ import { useQueryState } from "next-usequerystate"
 import { DatePicker } from "@mui/x-date-pickers"
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
-import dayjs from 'dayjs';
+import dayjs from "dayjs"
+import PersonAddIcon from "@mui/icons-material/PersonAdd"
+import GroupAddIcon from "@mui/icons-material/GroupAdd"
+import GroupRemoveIcon from "@mui/icons-material/GroupRemove"
 
 type ActivityType = "Run" | "Walk" | "Ride"
 
@@ -32,6 +39,9 @@ const HomePage = () => {
   const [startDate, setStartDate] = useQueryState("startDate")
   const [endDate, setEndDate] = useQueryState("endDate")
   const [error, setError] = useState<string | null>(null)
+  const [selectedAthletes, setSelectedAthletes] =
+    useQueryState("selectedAthletes")
+  const [dialogOpen, setDialogOpen] = useState<boolean>(false)
 
   const getRecords = useCallback(async () => {
     const downloadedRecords = await downloadRecords()
@@ -42,15 +52,22 @@ const HomePage = () => {
     setRecords(parsedRecords)
   }, [])
 
-  const getUniqueNames = useCallback(() => {
-    const names: string[] = []
-    records.forEach((record) => {
-      if (!names.includes(record.name)) {
-        names.push(record.name)
-      }
-    })
-    return names
-  }, [records])
+  const getUniqueNames = useCallback(
+    (excludeSelectedAthletes: boolean = false) => {
+      const names: string[] = []
+      const selectedNames = selectedAthletes?.split(",") ?? []
+      records.forEach((record) => {
+        if (
+          !names.includes(record.name) &&
+          (!excludeSelectedAthletes || !selectedNames.includes(record.name))
+        ) {
+          names.push(record.name)
+        }
+      })
+      return names.sort()
+    },
+    [records, selectedAthletes]
+  )
 
   const getTotal = useCallback(
     (
@@ -59,12 +76,15 @@ const HomePage = () => {
     ) => {
       let totalDistance: number = 0
       records.forEach((record) => {
-        console.log(endDate as string)
         if (
           record.name == name &&
           record.type == filter &&
           (!startDate || record.date >= new Date(startDate as string)) &&
-          (!endDate || record.date <= new Date(new Date(endDate as string).getTime() + 24 * 60 * 60 * 1000))
+          (!endDate ||
+            record.date <=
+              new Date(
+                new Date(endDate as string).getTime() + 24 * 60 * 60 * 1000
+              ))
         ) {
           totalDistance += record[field]
         }
@@ -77,20 +97,25 @@ const HomePage = () => {
   const getAggregates = useCallback(() => {
     const names = getUniqueNames()
     const aggregates: Aggregate[] = []
-    names.forEach((name) => {
-      const totalDistance = getTotal(name, "distance")
-      const totalElapsedTime = getTotal(name, "elapsedTime")
-      const totalElevationGain = getTotal(name, "totalElevationGain")
-      if (totalDistance > 0 || totalElapsedTime > 0)
-        aggregates.push({
-          name: name,
-          distance: totalDistance,
-          elapsedTime: totalElapsedTime,
-          totalElevationGain: totalElevationGain,
-        })
-    })
+    const selectedNames = selectedAthletes?.split(",") ?? []
+    names
+      .filter(
+        (name) => selectedAthletes === "" || selectedAthletes === null || selectedNames?.includes(name)
+      )
+      .forEach((name) => {
+        const totalDistance = getTotal(name, "distance")
+        const totalElapsedTime = getTotal(name, "elapsedTime")
+        const totalElevationGain = getTotal(name, "totalElevationGain")
+        if (totalDistance > 0 || totalElapsedTime > 0)
+          aggregates.push({
+            name: name,
+            distance: totalDistance,
+            elapsedTime: totalElapsedTime,
+            totalElevationGain: totalElevationGain,
+          })
+      })
     return aggregates
-  }, [getTotal, getUniqueNames])
+  }, [selectedAthletes, getTotal, getUniqueNames])
 
   useEffect(() => {
     getRecords()
@@ -114,13 +139,26 @@ const HomePage = () => {
   }, [tab, setTab, setTabToDefault])
 
   useEffect(() => {
-    // check start date is not after end date
+    // Check start date is not after end date
     if (startDate && endDate && dayjs(startDate) > dayjs(endDate)) {
       setError("Start date cannot be after end date")
     } else {
       setError(null)
     }
   }, [startDate, endDate])
+
+  const areAllAthletesSelected = useCallback(() => {
+    return (
+      (selectedAthletes?.split(",") ?? []).length === getUniqueNames().length
+    )
+  }, [selectedAthletes, getUniqueNames])
+
+  useEffect(() => {
+    // Close dialog box if all athletes are selected
+    if (areAllAthletesSelected()) {
+      setDialogOpen(false)
+    }
+  }, [selectedAthletes, areAllAthletesSelected])
 
   const recordRows = records.map((record, index) => {
     return {
@@ -180,6 +218,48 @@ const HomePage = () => {
     setTab(newValue.toString())
   }
 
+  const handleUnselectAthlete = (name: string) => {
+    setSelectedAthletes((currentValue) => {
+      const currentNames = currentValue ? currentValue.split(",") : []
+      return currentNames.filter((athlete) => athlete !== name).join(",")
+    })
+  }
+
+  const handleSelectAthlete = (name: string) => {
+    setSelectedAthletes((currentValue) => {
+      const currentNames = currentValue ? currentValue.split(",") : []
+      return !currentNames.includes(name)
+        ? [...currentNames, name].join(",")
+        : currentValue
+    })
+    setDialogOpen(false)
+  }
+
+  const handleSelectAllAthletes = () => {
+    setSelectedAthletes(getUniqueNames().join(","))
+  }
+
+  const handleUnselectAllAthletes = () => {
+    setSelectedAthletes("")
+  }
+
+  const renderSelectedAthleteChit = (name: string, index: number) => (
+    <Chip
+      key={index}
+      label={name}
+      onDelete={() => handleUnselectAthlete(name)}
+      className="bg-slate-500 text-white"
+      sx={{
+        "& .MuiChip-deleteIcon": {
+          color: "#94a3b8",
+          "&:hover": {
+            color: "#cbd5e1",
+          },
+        },
+      }}
+    />
+  )
+
   if (records.length === 0)
     return (
       <main className="flex flex-col h-screen w-screen justify-center items-center bg-slate-700">
@@ -191,7 +271,7 @@ const HomePage = () => {
     )
 
   return (
-    <main className="flex flex-col items-center p-5 h-screen box-border bg-slate-700">
+    <main className="flex flex-col items-center p-5 min-h-screen box-border bg-slate-700">
       <h1 className="text-2xl font-bold text-center m-0 mb-4 leading-none text-white">
         AutoRek Strava Club
       </h1>
@@ -217,8 +297,15 @@ const HomePage = () => {
                 sorting: {
                   sortModel: [{ field: "date", sort: "desc" }],
                 },
+                pagination: {
+                  paginationModel: { pageSize: 20 },
+                },
               }}
-              autoPageSize
+              sx={{
+                "& .MuiDataGrid-virtualScroller": {
+                  minHeight: "50px",
+                },
+              }}
             />
           </div>
         )}
@@ -240,43 +327,109 @@ const HomePage = () => {
                   </Select>
                 </FormControl>
               </div>
-              <div className="max-w-[259px]">
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    label="Start date"
-                    value={startDate ? dayjs(startDate) : null}
-                    onChange={(newValue) => setStartDate(newValue?.toString() ?? null)}
-                    slotProps={{
-                      field: {
-                        clearable: true,
-                        onClear: () => setStartDate(null),
-                      },
-                    }}
-                  />
-                </LocalizationProvider>
+              <div className="flex gap-4 flex-col sm:flex-row">
+                <div className="max-w-[259px]">
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      label="Start date"
+                      value={startDate ? dayjs(startDate) : null}
+                      onChange={(newValue) =>
+                        setStartDate(newValue?.toString() ?? null)
+                      }
+                      slotProps={{
+                        field: {
+                          clearable: true,
+                          onClear: () => setStartDate(null),
+                        },
+                      }}
+                    />
+                  </LocalizationProvider>
+                </div>
+                <div className="max-w-[259px]">
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      label="End date"
+                      value={endDate ? dayjs(endDate) : null}
+                      onChange={(newValue) =>
+                        setEndDate(newValue?.toString() ?? null)
+                      }
+                      slotProps={{
+                        field: {
+                          clearable: true,
+                          onClear: () => setEndDate(null),
+                        },
+                      }}
+                    />
+                  </LocalizationProvider>
+                </div>
               </div>
-              <div className="max-w-[259px]">
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    label="End date"
-                    value={endDate ? dayjs(endDate) : null}
-                    onChange={(newValue) => setEndDate(newValue?.toString() ?? null)}
-                    slotProps={{
-                      field: {
-                        clearable: true,
-                        onClear: () => setEndDate(null),
-                      },
-                    }}
-                  />
-                </LocalizationProvider>
-              </div>
+              {!selectedAthletes && (
+                <div>
+                  <IconButton
+                    onClick={() => setDialogOpen(true)}
+                    aria-label="filter athletes"
+                  >
+                    <PersonAddIcon></PersonAddIcon>
+                  </IconButton>
+                  <IconButton
+                    aria-label="select all athletes"
+                    onClick={handleSelectAllAthletes}
+                    disabled={areAllAthletesSelected()}
+                  >
+                    <GroupAddIcon fontSize="inherit" />
+                  </IconButton>
+                </div>
+              )}
             </div>
             {error && (
               <div className="self-center">
                 <p className="text-red-500 m-0">{error}</p>
               </div>
             )}
-            <div className="flex-1 box-border mt-3">
+            {selectedAthletes && (
+              <div className="flex flex-col lg:flex-row p-3 mt-2 gap-x-3 gap-y-2 items-center bg-slate-300 rounded">
+                <div className="self-center lg:self-start h-8 flex items-center">
+                  <p className="m-0 text-slate-700">Selected Athletes</p>
+                </div>
+                <div className="flex-1 flex flex-row gap-3 flex-wrap justify-center">
+                  {selectedAthletes
+                    .split(",")
+                    .map((name, index) =>
+                      renderSelectedAthleteChit(name, index)
+                    )}
+                </div>
+                <div className="self-center lg:self-start h-8 flex items-center">
+                  <IconButton
+                    aria-label="select athlete"
+                    onClick={() => setDialogOpen(true)}
+                    disabled={areAllAthletesSelected()}
+                  >
+                    <PersonAddIcon fontSize="inherit" />
+                  </IconButton>
+                  <IconButton
+                    aria-label="select all athletes"
+                    onClick={handleSelectAllAthletes}
+                    disabled={areAllAthletesSelected()}
+                  >
+                    <GroupAddIcon fontSize="inherit" />
+                  </IconButton>
+                  <IconButton
+                    aria-label="unselect all athletes"
+                    onClick={handleUnselectAllAthletes}
+                  >
+                    <GroupRemoveIcon fontSize="inherit" />
+                  </IconButton>
+                </div>
+              </div>
+            )}
+            <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+              {getUniqueNames(true).map((name, index) => (
+                <Button key={index} onClick={() => handleSelectAthlete(name)}>
+                  {name}
+                </Button>
+              ))}
+            </Dialog>
+            <div className="flex-1 box-border mt-2">
               <DataGrid
                 rows={aggregateRows}
                 columns={aggregateColumns}
@@ -284,8 +437,15 @@ const HomePage = () => {
                   sorting: {
                     sortModel: [{ field: "distance", sort: "desc" }],
                   },
+                  pagination: {
+                    paginationModel: { pageSize: 20 },
+                  },
                 }}
-                autoPageSize
+                sx={{
+                  "& .MuiDataGrid-virtualScroller": {
+                    minHeight: "50px",
+                  },
+                }}
               />
             </div>
           </div>
